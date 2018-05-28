f833da85148282dd23666ea921568d74370b4593
Felix Kahmann <felix@kahmann.de>
2018-05-28 11:03:47 +0200

    add route stubs for auth and secure

--- src/webserver/routes/index.js
+++ src/webserver/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('./auth');
 
+const secure = require('./secure');
+
+router.use('/auth', auth);
+router.use('/secure', secure);
 
 
 module.exports = router;
