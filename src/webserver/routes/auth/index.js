335c53729e6928cfa8995f1561f12345d3e10e65
Felix Kahmann <felix@kahmann.de>
2018-06-05 14:03:04 +0200

    add routes to auth

--- src/webserver/routes/auth/index.js
+++ src/webserver/routes/auth/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/login', (req, res) => {
+  res.sendStatus(501);
+});
 
+router.post('/register', (req, res) => {
+  res.sendStatus(501);
+});
+
+router.post('/refresh', (req, res) => {
+  res.sendStatus(501);
+});
 
 
 module.exports = router;
