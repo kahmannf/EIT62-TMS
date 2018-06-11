de556e778161c570e90939c506019b8087a63b67
Felix Kahmann <felix@kahmann.de>
2018-06-11 12:48:52 +0200

    implement registration methods

--- src/webserver/lib/auth.js
+++ src/webserver/lib/auth.js
@@ -40,8 +40,71 @@ const registerUser = (username, password) => new Promise((resolve, reject) => {
   .catch(reject);
 });
 
+/**
+ * checks whether the giver credentials are valid login creadentials
+ * @param {string} username 
+ * @param {string} password
+ * @returns {Promise} represents a boolean value which indicates whether the crediential are valid 
+ */
+const verifyCredentials = (username, password) => new Promise((resolve, reject) => {
+  var sql = 'select * from user where lower(username) like lower(?)';
 
+  username = username.trim();
+
+  db.get(sql, username, (err, row) => {
+    if(err) {
+      reject(err);
+    } else if (row) {
+
+      if(security.verifyPassword(password, row.Hash, row.Salt)){
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } else {
+      resolve(false);
+    }
+  });
+});
+
+/**
+ * Gets a user-object from the database
+ * @param {string} username 
+ * @returns a user-object without hash and salt properties
+ */
+const getUserByUsername = (username) => new Promise((resolve, reject) => {
+
+  username = username.trim();
+
+  var sql = 'select id, username from user where lower(username) like lower(?)';
+
+  db.get(sql, username, (err, row) => {
+    if(err) {
+      reject(err);
+    } else if (row) {
+      resolve(row);
+    } else {
+      reject(`no user with username '${username}'`);
+    }
+  });
+
+});
+
+/**
+ * Creates a jsonwebtoken for authentication containing the user as payload
+ * @param {string} username 
+ * @returns {string} a jsonwebtoken
+ */
+const getToken = (username) => new Promise((resolve, reject) => {
+  getUserByUsername(username)
+  .then(user => {
+    resolve(security.getToken(user)); 
+  })
+  .catch(reject);
+});
 
 module.exports = {
-  registerUser
+  getToken,
+  registerUser,
+  verifyCredentials
 }
