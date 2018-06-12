572926366f2f5d670ffa85071db41c75b49869a9
Felix Kahmann <felix@kahmann.de>
2018-06-12 14:04:11 +0200

    fix bug, start implemeting webserver

--- src/webserver/lib/projects.js
+++ src/webserver/lib/projects.js
@@ -1,2 +1,28 @@
 'use strict';
 const db = require('./db');
+
+const saveProject = (project, user) => new Promise((resolve, reject) => {
+  
+  
+  reject();
+});
+
+const getUserProjects = (user) => new Promise((resolve, reject) => {
+  let sql = 'select p.ID, p.Timestamp, p.Description, p.OwnerID from project p, projectmember pm where p.ID = pm.IDProject and (p.ownerid = ? or pm.iduser = ?)';
+
+  db.all(sql, user.ID, (err, rows) => {
+    if(err) {
+      reject(err);
+    } else if (rows) {
+      resolve(rows);
+    } else {
+      resolve([]);
+    }
+  });
+});
+
+
+module.exports = {
+  saveProject,
+  getUserProjects
+}
